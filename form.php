6d38ba6f7153c521d960aaebd0d28b121c1bdf56
Kotkusok <victer75@mail.ru>
2024-12-17 22:23:21 +0800

    update files

--- form.php
+++ form.php
@@ -1,3 +1,8 @@
 <?php    
-    echo "list";
+    <form action="query.php" method="POST">
+    login id 1: <input type="text" name="login1" size=10><br><br>
+    login id 2: <input type="text" name="login2" size=10><br><br>
+    login id 3: <input type="text" name="login3" size=10><br><br>
+    login id 4: <input type="text" name="login4" size=10><br><br>
+    <br><br><input type="submit" value="OK">
 ?>
--- list.php
+++ list.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<?php    
-    echo "list";
+<?php
+    error_reporting(0);
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = '1234';
+    $db_name = 'guest_book';
+    $link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+    if (!$link) {
+        die('<p style="color:red">'.mysqli_connect_errno().' - '.mysqli_connect_error().'</p>');
+    }
+    echo "<p>Вы подключились к MySQL!</p><br>";
+    $res = mysqli_query($link, "SELECT * FROM message");
+    if (!$res)
+    {
+        echo "error res\n";
+    //    exit;
+    }
+    else
+    {
+        echo "";
+    }
+    echo '<table border="1">
+        <tr>
+            <td>User Name</td>
+            <td>E-mail</td>
+            <td>Homepage</td>
+            <td>Text</td>
+            <td>IP</td>
+            <td>Browser</td>
+        </tr>
+        <tr>';
+    while ($row = mysqli_fetch_row($res))
+    {
+        for($i=0; $i<count($row); $i++)
+        {
+            echo "<td>".$row[$i]."</td>";
+        }
+    }
+    echo "</tr>
+        </table>";
 ?>
-</body>
-</html>
